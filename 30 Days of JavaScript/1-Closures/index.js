add20d533bc3210a8335af8d243e68ec320a64af
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-03 23:48:08 +0500

    feat: 2-Counter

--- 30 Days of JavaScript/1-Closures/index.js
+++ 30 Days of JavaScript/1-Closures/index.js
@@ -2,3 +2,8 @@
 // https://leetcode.com/problems/create-hello-world-function/
 
 let createHelloWorld = () => () => 'Hello World';
+
+// 2-Counter
+// https://leetcode.com/problems/counter/
+
+var createCounter=(n)=>{let counter=n;return()=>counter++}
